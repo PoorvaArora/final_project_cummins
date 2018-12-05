adeed42675ac2b1264a6a83013459f23acf9fb0e
PoorvaArora <pooarora@iu.edu>
2018-12-05 13:39:25 -0500

    sensorId url

--- public/js/client.app.js
+++ public/js/client.app.js
@@ -36,15 +36,15 @@ var agsApp = new Vue({
       }
     },
     checkDetails(sensorId){
-      window.location.replace("kpi.html");
-      fetch('api/sensorTimeSeries.php?sensorId='+sensorId)
-      .then( response => response.json() )
-      // ^ This is the same as .then( function(response) {return response.json()} )
-      .then( json => {kpiApp.sensorsDeployed = json})
-      .catch( err => {
-        console.log('TASK FETCH ERROR:');
-        console.log(err);
-      })
+      window.location.replace("kpi.html/"+sensorId);
+      // fetch('api/sensorTimeSeries.php?sensorId='+sensorId)
+      // .then( response => response.json() )
+      // // ^ This is the same as .then( function(response) {return response.json()} )
+      // .then( json => {kpiApp.sensorsDeployed = json})
+      // .catch( err => {
+      //   console.log('TASK FETCH ERROR:');
+      //   console.log(err);
+      // })
     }
   },
   created () {
