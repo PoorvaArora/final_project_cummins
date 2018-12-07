c01c036f69b0c344db6dd47594ff323c70c3a759
PoorvaArora <pooarora@iu.edu>
2018-12-07 09:30:23 -0500

    common.php

--- app/common.php
+++ app/common.php
@@ -14,10 +14,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'
 /** MODELS **/
 require 'models/Client.php';
 require 'models/ClientProduct.php';
-require 'models/Sensor.php';
-require 'models/SensorDeployed.php';
 require 'models/SensorTimeSeries.php';
-require 'models/Site.php';
-require 'models/Turbine.php';
-require 'models/TurbineDeployed.php';
 require 'models/Note.php';
